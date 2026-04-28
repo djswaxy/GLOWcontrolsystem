50850414b95b59f678dd17e51269c1906d96663e
Magne S <magnes05@outlook.dk>
2026-04-28 10:25:15 +0200

    Created the initial project for Arduino GLOW control

---
+++ Arduino/Arduino-GLOW-Control/Arduino-GLOW-Control/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Arduino-GLOW-Control.cpp
+ *
+ * Created: 28-04-2026 10:03:11
+ * Author : magne
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
