280e6e48c4cab1290d4e9acc7d48684ab4772f90
Oscar <au800853@uni.au.dk>
2026-04-30 12:46:02 +0200

    c fil funktioner

--- Arduino/Arduino-GLOW-Control/src/communicationHandler.c
+++ Arduino/Arduino-GLOW-Control/src/communicationHandler.c
@@ -1,2 +1,22 @@
 
 #include "communicationHandler.h"
+
+
+
+ISR(){
+
+}
+
+void initCommunication() {
+    //DDRE |= 0x00000010;
+	//DDRE &= 0x11111110;
+	sei();
+}
+
+void TransmitData(unsigned char message_id, unsigned char data) {
+
+ }
+
+void Receiver(unsigned char message_id) {
+
+}
