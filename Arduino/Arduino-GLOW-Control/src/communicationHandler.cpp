592e4753a3a756995ed2e2578bfa7906beffd30d
Magne S <magnes05@outlook.dk>
2026-05-15 11:51:30 +0200

    Refactor serial packet parsing and buffering
    Replace the old received_bytes buffer with explicit message state (messageBegun, messageID, messageDATA) and simplify ISR parsing. ISR now looks for 0xAA start, reads a message ID, collects up to 8 data bytes, and checks for 0xBB end; rx_index bounds tightened to avoid overflow and malformed packets call ClearReceivedMessage. Updated Parrot() and Receiver() to use messageID/messageDATA, and added ClearReceivedMessage() to reset parsing state (messageDATA, messageBegun, messageID, rx_index, packet_received). Overall fixes packet handling and reduces risk of buffer overrun.

--- Arduino/Arduino-GLOW-Control/src/communicationHandler.cpp
+++ Arduino/Arduino-GLOW-Control/src/communicationHandler.cpp
@@ -15,29 +15,39 @@ extern unsigned int PasserbyMAHAmount;
 extern unsigned int PasserbyWeek;
 extern unsigned int PasserbyAllTime;
 
-volatile unsigned char received_bytes[11];
+volatile unsigned char messageBegun = 0;
+volatile unsigned char messageID = 0;
+volatile unsigned char messageDATA[8];
 volatile unsigned char rx_index = 0;
 volatile unsigned char packet_received = 0;
 
 ISR(USART0_RX_vect) {
     toggleLED(4);
-    //TransmitData(0x00, 0x01);
 	unsigned char data = UDR0;
     
-	if (rx_index == 0 && data != 0xAA) return;
-	
-	
-	if (rx_index < 11) { // no overflow, added by djswaxy
-        received_bytes[rx_index] = data;
-        rx_index++;
+    if (rx_index == 0) {
+        if (data == 0xAA) {
+            messageBegun = 1;
+            return;
+        }
+        else if (messageBegun && !messageID) {
+            messageID = data;
+            return;
+        }
     }
-	if (rx_index == 11) {
-		if (received_bytes[10] == 0xBB) {
+	
+	if (rx_index < 8) {
+        messageDATA[rx_index] = data;
+        rx_index++;
+        return;
+    }
+	if (rx_index == 8) {
+		if (data == 0xBB) {
 			toggleLED(6);
 			packet_received = 1;
 		}
         else { //handler for packets som blir fucked
-            rx_index = 0;
+            ClearReceivedMessage();
         }
 	} 
 }
@@ -69,16 +79,16 @@ void TransmitData(unsigned char message_id, unsigned char* data) {
 	}
 }
 void Parrot() {
-    unsigned char ParrotSettingID = received_bytes[1];
+    unsigned char ParrotSettingID = messageID;
     unsigned char ParrotSettingDATA[8] = {};
     for (int i = 0; i < 8; i++) {
-        ParrotSettingDATA[i] = received_bytes[i+2];
+        ParrotSettingDATA[i] = messageDATA[i];
     }
     TransmitData(ParrotSettingID,ParrotSettingDATA);
 
 }
 void Receiver() {
-    switch(received_bytes[1]){
+    switch(messageID){
         case 0xCC:
         { // PC wants to connect
             // PC AND ARDUINO ARE NOW CONNECTED
@@ -86,10 +96,10 @@ void Receiver() {
         }
         case 0xAC: 
         {
-            unsigned int newSens    = (received_bytes[2] << 8) | received_bytes[3];
-            unsigned int newDist     = (received_bytes[4] << 8) | received_bytes[5];
-            unsigned int newMaxL    = (received_bytes[6] << 8) | received_bytes[7];
-            unsigned int newStandby = (received_bytes[8] << 8) | received_bytes[9];
+            unsigned int newSens    = (messageDATA[0] << 8) | messageDATA[1];
+            unsigned int newDist    = (messageDATA[2] << 8) | messageDATA[3];
+            unsigned int newMaxL    = (messageDATA[4] << 8) | messageDATA[5];
+            unsigned int newStandby = (messageDATA[6] << 8) | messageDATA[7];
             mvtSensor = newSens;
             dist = newDist;
             activeLight = newMaxL;
@@ -135,13 +145,16 @@ void Receiver() {
             Parrot();
             break;    
     }
+    ClearReceivedMessage();
 }
 
 
 void ClearReceivedMessage(){
-    for (unsigned char i = 0; i < 11; i++) {
-	received_bytes[i] = 0;
+    for (unsigned char i = 0; i < 8; i++) {
+	    messageDATA[i] = 0;
 	}
+    messageBegun = 0;
+    messageID = 0;
 	rx_index = 0;
 	packet_received = 0;
 }
